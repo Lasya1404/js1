4e66adbc50031de4fb7afec742a12c7260448359
lasya <126437621+Lasya1404@users.noreply.github.com>
2023-10-30 10:53:34 +0000

    basics

---
+++ basicsOfJs/iterations/foreach.js
@@ -0,0 +1,56 @@
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// // coding.forEach( function (val){
+// //     console.log(val);
+// // } )
+
+// // coding.forEach( (item) => {
+// //     console.log(item);
+// // } )
+
+// // function printMe(item){
+// //     console.log(item);
+// // }
+
+// // coding.forEach(printMe)
+
+// // coding.forEach( (item, index, arr)=> {
+// //     console.log(item, index, arr);
+// // } )
+
+// const myCoding = [
+//     {
+//         languageName: "javascript",
+//         languageFileName: "js"
+//     },
+//     {
+//         languageName: "java",
+//         languageFileName: "java"
+//     },
+//     {
+//         languageName: "python",
+//         languageFileName: "py"
+//     },
+// ]
+
+// myCoding.forEach( (item) => {
+    
+//     console.log(item.languageName);
+// } )
+const namee=[
+    {name1:"lasya",
+    age:20
+    },
+    {name2:"lasya",
+    age:20
+    },
+    {name2:"lasya",
+    age:20
+    }
+]
+// for (let w=0;w<namee.length;w++){
+//     console.log(namee[w].age);
+// }
+namee.forEach(element => {
+    console.log(element.age);
+});
